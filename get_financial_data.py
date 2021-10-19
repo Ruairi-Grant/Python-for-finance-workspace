8a67783d18a64f3f50149c03075401a68a73ebac
Ruairi-Grant <grantru@tcd.ie>
2021-10-19 21:42:48 +0100

    Transactions view started

--- get_financial_data.py
+++ get_financial_data.py
@@ -2,7 +2,9 @@
 from yahoofinancials import YahooFinancials
 import pandas as pd
 
-
+#DataFrame Setup
+transaction_columns = ['Date', 'Ticker', 'Type', 'Units', 'Price Per Unit', 'Total Price']
+transaction_df = pd.DataFrame(columns= transaction_columns)
 
 user_input = input(">>>")
 
@@ -24,9 +26,34 @@ while (user_input != "Quit"):
         print(df)
 
     if (user_input == 'Enter Transactions'): #Allow user to enter past transactions and generate relevant dataframes #TODO #1
-        print("Feature not ready.\n")
+        while(user_input == 'Enter Transactions'):
+            trans_type = input("Enter transaction type.\n>>> ")
+            ticker = input("Enter stock ticker\n>>> ")
+            units = input("Enter number of units\n>>> ")
+            ppu = input("Enter price per unit in USD\n>>> ") #TODO #4
+            date = input("Enter trans date in format DD-MM-YYYY\n>>> ")
+            total_price = int(units)*int(ppu)
+            transaction = {'Date':date, 'Ticker':ticker, 'Type':trans_type, 'Units':units, 'Price Per Unit':ppu, 'Total Price':total_price}
+            print("Confirm Transaction: y/n\n",transaction,"\n")
+            choice = input(">>>")
+            if (choice == 'y'):
+                transaction_df = transaction_df.append(transaction, ignore_index= True)
+                choice = input("Would you like to enter another transaction: y/n\n")
+                if (choice == 'y'):
+                    user_input = 'Enter Transactions'
+                
+                elif (choice == 'n'):
+                    user_input = '0'
+
+
+            elif (choice == 'n'):
+                user_input = 'Enter Transactions'
+
+        print(transaction_df)
+        
+
 
     if (user_input == 'Portfolio Eval'): #Output evaluation of overall portfolio #TODO #3
-        print("Feature not ready yet")
+        print("Feature not ready yet\n")
 
-    user_input = input("Please choose and option:\n1:Current Price\n2:ratios and common indicators\n3:Plot of 5 year stock price\nType \"Quit\" to quit")
+    user_input = input(">>> ")

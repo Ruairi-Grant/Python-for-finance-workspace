f473b88a682f0842162ee4726b7d56fad04774ea
Ruairi-Grant <grantru@tcd.ie>
2021-10-19 20:23:44 +0100

    Change setup slighly, nicer interface

--- get_financial_data.py
+++ get_financial_data.py
@@ -3,27 +3,30 @@ from yahoofinancials import YahooFinancials
 import pandas as pd
 
 
-tickers = input("Please enter your tickers seperated by a space: ").split()
-user_input = input("Please choose an option:\n1:Current Price\n2:Ratios and Common Indicators\n3:Plot of 5 year stock price\nType \"Quit\" to quit\n")
 
-yf = YahooFinancials(tickers)
+user_input = input(">>>")
+
 
 while (user_input != "Quit"):
-  if (user_input == '1'):  #output the current trading day data
-    #set up names
-    standard_data = ["regularMarketPrice", "regularMarketOpen", "regularMarketDayHigh", "regularMarketDayLow"]
-    standard_data_names = ["Current Price", "Open", "Close", "Low"]
-    #get data from Yahoo    
-    curr_price = yf.get_stock_price_data()
-    #turn dict into a nicly formatted DataFrame and print
-    df = pd.DataFrame.from_dict(curr_price)
-    df = df.transpose()
-    df = df[standard_data]
-    df.rename(columns = dict(zip(standard_data, standard_data_names)), inplace = True)
-    print(df)
+    if (user_input == 'Stocks Eval'):  #output the current trading day data #TODO #2 
+        tickers = input("Please enter your tickers seperated by a space: ").split()
+        yf = YahooFinancials(tickers)
+        #set up names
+        standard_data = ["regularMarketPrice", "regularMarketOpen", "regularMarketDayHigh", "regularMarketDayLow"]
+        standard_data_names = ["Current Price", "Open", "Close", "Low"]
+        #get data from Yahoo    
+        curr_price = yf.get_stock_price_data()
+        #turn dict into a nicly formatted DataFrame and print
+        df = pd.DataFrame.from_dict(curr_price)
+        df = df.transpose()
+        df = df[standard_data]
+        df.rename(columns = dict(zip(standard_data, standard_data_names)), inplace = True)
+        print(df)
 
-  if (user_input == '2'): #utput a tabel of common stock indicators
-    standard_data = ["regularMarketPrice", "regularMarketOpen", "regularMarketDayHigh", "regularMarketDayLow"]
+    if (user_input == 'Enter Transactions'): #Allow user to enter past transactions and generate relevant dataframes #TODO #1
+        print("Feature not ready.\n")
 
+    if (user_input == 'Portfolio Eval'): #Output evaluation of overall portfolio #TODO #3
+        print("Feature not ready yet")
 
-  user_input = input("Please choose and option:\n1:Current Price\n2:ratios and common indicators\n3:Plot of 5 year stock price\nType \"Quit\" to quit")
+    user_input = input("Please choose and option:\n1:Current Price\n2:ratios and common indicators\n3:Plot of 5 year stock price\nType \"Quit\" to quit")

b5ea15fdc7d32e3c0efd32430efe8a2a815b5515
Ruairi-Grant <grantru@tcd.ie>
2022-05-10 20:55:46 +0100

    Template for 3 primary views

--- tk_gui.py
+++ tk_gui.py
@@ -5,6 +5,60 @@ from tkinter import *
 from tkinter import ttk
 
 
+class StockEval:
+
+    def __init__(self):
+        t = Toplevel()
+        t.title("Stock Evaluation")
+
+        stockEvalFrame = ttk.Frame(t, padding="3 3 12 12")
+        stockEvalFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+        t.columnconfigure(0, weight=1)
+        t.rowconfigure(0, weight=1)
+
+        ticker = StringVar()
+        ticker_entry = ttk.Entry(stockEvalFrame, width=7, textvariable=ticker)
+        ticker_entry.grid(column=2, row=1, sticky=(W, E))
+
+        ttk.Label(stockEvalFrame, text="feet").grid(column=3, row=1, sticky=W)
+
+
+class PortfolioEval:
+
+    def __init__(self):
+        t = Toplevel()
+        t.title("Portfolio Evaluation")
+
+        portfolioEvalFrame = ttk.Frame(t, padding="3 3 12 12")
+        portfolioEvalFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+        t.columnconfigure(0, weight=1)
+        t.rowconfigure(0, weight=1)
+
+        ticker = StringVar()
+        ticker_entry = ttk.Entry(portfolioEvalFrame, width=7, textvariable=ticker)
+        ticker_entry.grid(column=2, row=1, sticky=(W, E))
+
+        ttk.Label(portfolioEvalFrame, text="XXX").grid(column=3, row=1, sticky=W)
+
+
+class EditTransaction:
+
+    def __init__(self):
+        t = Toplevel()
+        t.title("Edit transactions")
+
+        transactionEditFrame = ttk.Frame(t, padding="3 3 12 12")
+        transactionEditFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+        t.columnconfigure(0, weight=1)
+        t.rowconfigure(0, weight=1)
+
+        ticker = StringVar()
+        ticker_entry = ttk.Entry(transactionEditFrame, width=7, textvariable=ticker)
+        ticker_entry.grid(column=2, row=1, sticky=(W, E))
+
+        ttk.Label(transactionEditFrame, text="YYY").grid(column=3, row=1, sticky=W)
+
+
 class PortfolioGUI:
 
     def __init__(self, root):
@@ -16,28 +70,9 @@ class PortfolioGUI:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        ttk.Button(mainframe, text="Stock Analysis", command=self.stockEval).grid(column=1, row=3, sticky=W)
-        ttk.Button(mainframe, text="Edit Transactions", command=self.editTrans).grid(column=2, row=3, sticky=W)
-        ttk.Button(mainframe, text="Portfolio Analysis", command=self.portfolioEval).grid(column=3, row=3, sticky=W)
-
-    def calculate(self, *args):
-        try:
-            value = float(self.feet.get())
-            self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-        except ValueError:
-            pass
-
-    def stockEval(self):
-        t = Toplevel()
-        t.title("Stock Evaluation")
-
-    def editTrans(self):
-        t = Toplevel()
-        t.title("Edit Transactions")
-
-    def portfolioEval(self):
-        t = Toplevel()
-        t.title("Portfolio Evaluation")
+        ttk.Button(mainframe, text="Stock Analysis", command=StockEval).grid(column=1, row=3, sticky=W)
+        ttk.Button(mainframe, text="Edit Transactions", command=EditTransaction).grid(column=2, row=3, sticky=W)
+        ttk.Button(mainframe, text="Portfolio Analysis", command=PortfolioEval).grid(column=3, row=3, sticky=W)
 
 
 root = Tk()
